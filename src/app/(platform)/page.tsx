c5adb439823416ead1914d24cc2cd406fd1e8220
Josh Pinto <joshpinto6@outlook.com>
2025-09-07 20:01:25 -0400

    :speech_balloon: Rename "vibe check" --> "vibecheck"

--- src/app/(platform)/page.tsx
+++ src/app/(platform)/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="flex flex-col min-w-md max-w-md gap-4 px-8">
           <div className="flex flex-row gap-4">
             <h1 className="text-3xl text-rose-500 dark:text-rose-400 font-heading">
-              Welcome to vibe check
+              Welcome to vibecheck
             </h1>
             <Star40
               className="text-rose-500 dark:text-rose-400 motion-safe:animate-spin"
@@ -26,7 +26,7 @@ export default function Home() {
             />
           </div>
           <p>
-            vibe check is the place to be for creating and sharing personality
+            vibecheck is the place to be for creating and sharing personality
             quizzes, whether a fun quiz for your friends or a more intensive,
             calculated assessment.
           </p>
